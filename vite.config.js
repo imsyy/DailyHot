05bb93a73b7e7fc98545bb7ac28cea4e29e6c301
Rayton <45968073+RaytonX@users.noreply.github.com>
2023-12-24 23:05:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -49,7 +49,7 @@ export default defineConfig({
         ],
       },
       manifest: {
-        name: "今日热榜",
+        name: "书友会热榜",
         short_name: "DailyHot",
         description: "汇聚全网热点，热门尽览无余",
         display: "standalone",
